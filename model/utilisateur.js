6d1b1798d599a7330f96e76b3b5291e13a6589a9
James <72478345+wikssjs@users.noreply.github.com>
2023-11-27 11:00:39 -0500

    fix bcrypt

--- model/utilisateur.js
+++ model/utilisateur.js
@@ -1,5 +1,6 @@
 import { promesseConnexion } from './connexion.js';
-import { hash } from 'bcrypt';
+import pkg from 'bcryptjs';
+const { hash } = pkg;
 
 export const addUtilisateur = async (username, password) => {
     let connexion = await promesseConnexion;
